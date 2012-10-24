3f357df0ac518329c64b2cbdb15d988641691ea6
root <root@beaglebone.(none)>
2012-10-24 05:25:08 +0000

    Actually commiting the changes to the mini project 2 files

--- main.c
+++ main.c
@@ -3,45 +3,102 @@
 Port Congiguration:
 -Encoder A: GPIO1_6 - Header P8, pin 3 - GPIO 38
 -Encoder B: GPIO1_15 - Header P8, pin 15 - GPIO 47
+-Encoder GND: Header P8, pin 1
+
 -Pushbutton Switch: GPIO1_16 - Header P9, pin 15 - GPIO 48
+-Pusshbuton Switch V+: Header P9, pin 3
 
 This program keeps track of an encoder. CW rotation will
 increment the ticks, and CCW will decrement it. The current
 number of ticks is printed when the button is pressed.
 
-The gpio.h header consists of the GPIO methods copied directly from:
+The files gpio.h and gpio.c  consist of the GPIO methods copied
+directly from:
 https://www.ridgerun.com/developer/wiki/index.php/Gpio-int-test.c
 
 */
 
-
+#include "gpio.h"
 #include <signal.h>
 #include <poll.h>
-#include "gpio.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define OMAP_DIR "/sys/kernel/debug/omap_mux"
 
 int running = 1;
 
+// Quit when ^C is pressed
 void signal_handler(int sig)
 {
+	printf("\n");
 	running = 0;
 }
 
 int main(int argc, char* argv[])
 {
+	// Polling variables
 	int rc;
 	struct pollfd fdset[3];
 	int nfds = 3;
 	int len;
 	char* buf[MAX_BUF];
-	int i;
 
-	int pos = 0;
+	// Keeps track of encoder ticks.
+	int ticks = 0;
 
+	// Variables used to store GPIO values
 	unsigned int enc_a_val;
 	unsigned int enc_b_val;
+	unsigned int sw_val;
 
 	// Handle Ctrl^C
 	signal(SIGINT, signal_handler);
+
+        // Configure the pins to use internal pull-down resistors
+        FILE *mux_ptr;
+        char pullup_str[10];
+	char pulldown_str[10];
+        strcpy(pullup_str, "0x0037");
+	strcpy(pulldown_str, "0x0027");
+
+        // Configure pull-up for the A signal
+        if ((mux_ptr = fopen(OMAP_DIR "/gpmc_ad6", "rb+")) == NULL)
+        {
+                printf("Failed to open gpmc_ad6. Quitting.\n");
+                exit(1);
+        }
+        else
+	{
+                fwrite(&pullup_str, sizeof(char), 6, mux_ptr);
+                fclose(mux_ptr);
+        }
+
+        // Configure pull-up for the B signal
+        if ((mux_ptr = fopen(OMAP_DIR "/gpmc_ad15", "rb+")) == NULL)
+        {
+                printf("Failed to open gpmc_ad15. Quitting.\n");
+                exit(1);
+        }
+        else
+	{
+                fwrite(&pullup_str, sizeof(char), 6, mux_ptr);
+                fclose(mux_ptr);
+        }
+
+        // Configure pull-down for the switch
+        if ((mux_ptr = fopen(OMAP_DIR "/gpmc_a0", "rb+")) == NULL)
+        {
+                printf("Failed to open gpmc_a0. Quitting.\n");
+                exit(1);
+        }
+        else
+	{
+                fwrite(&pulldown_str, sizeof(char), 6, mux_ptr);
+                fclose(mux_ptr);
+        }
 
 	// Set up GPIO pins
 	unsigned int gpio_a = 38;
@@ -52,20 +109,21 @@ int main(int argc, char* argv[])
 	int enc_b_fd;
 	int enc_sw_fd;
 
-	gpio_export(gpio_a);
-	gpio_export(gpio_b);
-	gpio_export(gpio_sw);
+	printf("Exporting a... %d\n", gpio_export(gpio_a));
+	printf("Exporting b... %d\n", gpio_export(gpio_b));
+	printf("Exporting sw... %d\n", gpio_export(gpio_sw));
 
-	gpio_set_dir(gpio_a, 0);
-	gpio_set_dir(gpio_b, 0);
-	gpio_set_dir(gpio_sw, 0);
+	printf("Setting a direction... %d\n", gpio_set_dir(gpio_a, 0));
+	printf("Setting b direction... %d\n", gpio_set_dir(gpio_b, 0));
+	printf("Setting sw direction... %d\n", gpio_set_dir(gpio_sw, 0));
 
 	// Interrupts
-	gpio_set_edge(gpio_a, "rising");
-	gpio_set_edge(gpio_sw, "rising");
+	printf("setting edge a... %d\n", gpio_set_edge(gpio_a, "rising"));
+	printf("setting edge sw... %d\n", gpio_set_edge(gpio_sw, "rising"));
 
 	// Open the file for the encoder A signal
 	enc_a_fd = gpio_fd_open(gpio_a);
+	enc_sw_fd = gpio_fd_open(gpio_sw);
 
 	// Main loop
 	while (running == 1)
@@ -83,6 +141,7 @@ int main(int argc, char* argv[])
 
 		rc = poll(fdset, nfds, -1);
 
+
 		if (rc < 0)
 		{
 //			printf("poll() failed.\n");
@@ -99,7 +158,6 @@ int main(int argc, char* argv[])
 		}
 
 		// Encoder click
-
 		if (fdset[1].revents & POLLPRI)
 		{
 			lseek(fdset[1].fd, 0, SEEK_SET);
@@ -110,18 +168,25 @@ int main(int argc, char* argv[])
 
 			if (enc_a_val == 1) // rising edge
 			{
-				if (enc_b_val == 0) pos--;
-				else pos++;
+				if (enc_b_val == 0) ticks--;
+				else ticks++;
 			}
 
 		}
 
+		// Button press - print the current number of encoder ticks
 		if (fdset[2].revents & POLLPRI)
 		{
 			lseek(fdset[2].fd, 0, SEEK_SET);
 			len = read(fdset[2].fd, buf, MAX_BUF);
 
-			printf("Ticks: %d\n", pos);
+			// Very simple debouncing
+			usleep(5000);
+			gpio_get_value(gpio_sw, &sw_val);
+			if (sw_val == 1)
+			{
+				printf("Ticks: %d\n", ticks);
+			}
 		}
 
 	}
